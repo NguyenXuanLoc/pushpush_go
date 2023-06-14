e606f63425f5c7d124c9fe0209c3ace15bf50db2
NguyenXuanLoc <59056264+NguyenXuanLoc@users.noreply.github.com>
2023-06-14 14:47:30 +0700

    Initial commit

---
+++ example/android/app/src/main/java/com/pushwoosh/demoapp2/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.pushwoosh.demoapp2
+
+import io.flutter.embedding.android.FlutterActivity;
+
+class MainActivity(): FlutterActivity() {
+
+}
---
+++ example/android/app/src/main/java/com/pushwoosh/demoapp2/MainApp.java
@@ -0,0 +1,7 @@
+package com.pushwoosh.demoapp2;
+
+import android.app.Application;
+
+public class MainApp extends Application {
+
+}
